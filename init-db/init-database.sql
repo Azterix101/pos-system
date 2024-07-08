0793e27c23f90cb48c6fc6a7da2f2ec278bd46cb
mashegon01 <Neo.Mashego2@vodacom.co.za>
2024-07-08 16:08:56 +0200

    COMMIT-0006: This improved version includes character set and collation specifications, stronger password recommendations, specific host definitions, and more granular privilege grants. The aim to enhance security and adherence to the principle of least privilege.

--- init-db/init-database.sql
+++ init-db/init-database.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS pos_system;
+CREATE DATABASE IF NOT EXISTS pos_system CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Check if the user exists, and if not, create the user and grant privileges
-CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'user_password';
-GRANT ALL PRIVILEGES ON pos_system.* TO 'user'@'%';
+CREATE USER IF NOT EXISTS 'user'@'localhost' IDENTIFIED BY 'secure_password_here';
+GRANT SELECT, INSERT, UPDATE, DELETE ON pos_system.* TO 'user'@'localhost';
 FLUSH PRIVILEGES;
